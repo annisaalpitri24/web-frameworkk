54643da248316415e5eebc21d5fdd8faccf5ef26
annisaalpitri24 <annisa62144@sma.belajar.id>
2026-03-31 09:11:41 +0700

    Update dosen.blade.php

--- resources/views/akademik/dosen.blade.php
+++ resources/views/akademik/dosen.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.main')
 @section('title', 'Data Dosen')
 
-@section('container')
+@section('content')
 <h1>Daftar Dosen</h1>
 <ol>
     @forelse ($dosen as $namaDosen)
     <li>{{ $namaDosen }}</li>
     @empty
         <div class="alert alert-warning d-inline-block">
-            Data tidak ada..Silahkan isi array untuk data dosen !</div>
+            Data tidak ada..Silahkan isi array untuk data dosen !
+        </div>
     @endforelse
 </ol>
 @endsection
