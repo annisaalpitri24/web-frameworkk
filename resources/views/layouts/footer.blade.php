c17159414c24efeccc73e5e0fb2c1d4849c192bd
annisaalpitri24 <annisa62144@sma.belajar.id>
2026-03-31 09:11:50 +0700

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-dark mt-auto py-3 text-white">
     <div class="container">
-        Copyright &copy; {{ date("Y")}} PNP
+        Copyright &copy; {{ date("Y")}} <a href="{{ url('pnp/jurusan/prodi') }}" >PNP</a>
     </div>
 </footer>
