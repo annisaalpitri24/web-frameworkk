f65ef3eccb37da4e7d5cf5e9fdfe6d6ed2fa6234
annisaalpitri24 <annisa62144@sma.belajar.id>
2026-04-07 15:31:54 +0700

    Update MahasiswaController.php

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaController extends Controller
 {
@@ -10,4 +11,19 @@ class MahasiswaController extends Controller
     {
         return "Ini adalah method index di MahasiswaController";
     }
+     public function insertSql()
+    {
+        $query = DB::insert("INSERT INTO students(nim,nama_lengkap,tempat_lahir,
+        tgl_lahir,email,prodi,alamat,created_at,updated_at) VALUES ('2401092002',
+        'Annisa Alpitri','Sawahlunto','2005-12-26','annisa@linux.org','MI',
+        'Jl. Sudirman no.10 Padang',now(),now())");
+    }
+    public function insertPrepared()
+{
+    $query=DB::insert("INSERT INTO students(nim,nama_lengkap,tempat_lahir,
+    tgl_lahir,email,prodi,alamat,created_at,updated_at) VALUES 
+    (?,?,?,?,?,?,?,?,?)",['2022090908','Taylor Otwel','Limau Manis',
+    '1971-08-12','taylor@laravel.com','MI','Jl. M Hatta no.1 Padang',now(),
+    now()]);
+}
 }
