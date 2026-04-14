d4a2432cd7ba9a8a7c61c0533c00dd70e2fcb0b2
annisaalpitri24 <annisa62144@sma.belajar.id>
2026-04-14 15:20:08 +0700

    Update mahasiswa.blade.php

--- resources/views/akademik/mahasiswa.blade.php
+++ resources/views/akademik/mahasiswa.blade.php
@@ -1,12 +1,25 @@
 @extends('layouts.main')
 @section('title')
+@section('navMhs','active')
 
 @section('content')
-<h1>Daftar Mahasiswa jurusan TI</h1>
-<ol>
-    @foreach ($mhs as $namaMhs)
-    <li>{{ $namaMhs }}</li>
-    @endforeach
-
-</ol>
+<h1>Daftar Mahasiswa Jurusan TI</h1>
+<table class="table table-bordered">
+    <tr>
+        <th>No</th>
+        <th>Nim</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Alamat</th>
+    </tr>
+@foreach ($mahasiswas as $mahasiswa)
+    <tr>
+        <td>{{ $mahasiswa->id }}</td>
+        <td>{{ $mahasiswa->nim }}</td>
+        <td>{{ $mahasiswa->nama_lengkap }}</td>
+        <td>{{ $mahasiswa->email }}</td>
+        <td>{{ $mahasiswa->alamat }}</td>
+    </tr>
+@endforeach
+</table>
 @endsection
