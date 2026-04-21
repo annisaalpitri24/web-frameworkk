e57745411925dcdbc2e382ae73a787c9c2533b8d
annisaalpitri24 <annisa62144@sma.belajar.id>
2026-04-21 11:47:22 +0700

    Update MahasiswaController.php

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -143,6 +143,136 @@ public function selectView()
         $mahasiswa->tgl_lahir = '2001-01-01';
         $mahasiswa->save();
 
-        dump($mahasiswa);
+        dd($mahasiswa);
     }
+
+    public function updateWhere()
+    {
+        $mahasiswa = Mahasiswa::where('nim','2401092002')->first();
+        $mahasiswa->alamat = 'Padang';
+        $mahasiswa->save();
+
+        dd($mahasiswa);
+    }
+    public function massUpdate()
+    {
+        $mahasiswa = Mahasiswa::where('nim','2401092002')->first()->update(
+            [
+                'tempat_lahir' => 'Padang',
+                'prodi' => 'Manajeman Informatika'
+
+            ]);
+        
+        dd($mahasiswa);
+    }
+    public function deleted()
+    {
+        $mahasiswa=Mahasiswa::find(4);
+        $mahasiswa->delete();
+
+        dd($mahasiswa);
+    }
+     public function destroy()
+    {
+        $mahasiswa=Mahasiswa::find(4);
+
+        dd($mahasiswa);
+    }
+    public function massDelete()
+    {
+        $mahasiswa = Mahasiswa::where('prodi','manajeman informatika')->delete();
+        
+        dd($mahasiswa);
+    }
+     //public function all()
+   // {
+     //   $mahasiswa=Mahasiswa::all();
+     //   echo $mahasiswa[0]->id . '<br>';
+     //   echo $mahasiswa[0]->nim . '<br>';
+     //   echo $mahasiswa[0]->nama_lengkap . '<br>';
+     //   echo $mahasiswa[0]->tempat_lahir . '<br>';
+     //   echo $mahasiswa[0]->alamat . '<br>';
+//
+     //   //dd($mahasiswa);
+    //}
+
+    public function all()
+   {
+     $mahasiswa=Mahasiswa::all();
+     foreach($mahasiswa as $mhs){
+
+        echo $mhs->id . '<br>';
+        echo $mhs->nim . '<br>';
+        echo $mhs->nama_lengkap . '<br>';
+        echo $mhs->tempat_lahir . '<br>';
+        echo $mhs->alamat;
+        echo '<hr>';
+   //dd($mahasiswa);
+     }
+   }
+
+   public function allView()
+   {
+    $mahasiswas=Mahasiswa::all();
+    return view('akademik.mahasiswa',['mahasiswas'=>$mahasiswas]);
+   }
+   public function getWhere()
+   {
+    $mahasiswas=Mahasiswa::where('prodi','MI')
+    ->orderBy('nama_lengkap','asc')
+    ->get();
+    return view('akademik.mahasiswa',['mahasiswas'=>$mahasiswas]);
+   }
+   public function first()
+   {
+    $mahasiswas=Mahasiswa::where('prodi','MI')->first();
+    return view('akademik.mahasiswa',['mahasiswas'=>[$mahasiswas]]);
+   }
+   public function find()
+   {
+    $mahasiswas=Mahasiswa::find(2);
+    return view('akademik.mahasiswa',['mahasiswas'=>[$mahasiswas]]);
+   }
+   public function latest()
+   {
+    $mahasiswas=Mahasiswa::latest()->get();
+    return view('akademik.mahasiswa',['mahasiswas'=>$mahasiswas]);
+   }
+   public function limit()
+   {
+    $mahasiswas=Mahasiswa::latest()->limit(2)->get();
+    return view('akademik.mahasiswa',['mahasiswas'=>$mahasiswas]);
+   }
+   public function skiptake()
+   {
+    $mahasiswas=Mahasiswa::orderBy('id')->skip(1)->take(2)->get();
+    return view('akademik.mahasiswa',['mahasiswas'=>$mahasiswas]);
+   }
+   public function softDelete()
+   {
+    Mahasiswa::where('id','3')->delete();
+    return("Data Berhasil Dihapus");
+   }
+   public function withTrashed()
+   {
+    $mahasiswas=Mahasiswa::withTrashed()->get();
+    return view('akademik.mahasiswa',['mahasiswas'=>$mahasiswas]);
+   }
+   public function restore()
+   {
+    Mahasiswa::withTrashed()->where('id','3')->restore();
+    return 'Berhasil di restore';
+   }
+   public function forceDelete()
+   {
+    Mahasiswa::where('id','3')->forceDelete();
+    return('Data Berhasil dihapus secara permanen');
+   }
+   public function indexs()
+   {
+    $mahasiswas=Mahasiswa::latest()->paginate(5);
+    return view('akademik.mahasiswa',['mahasiswas'=>$mahasiswas]);
+   }
+
+
 }
